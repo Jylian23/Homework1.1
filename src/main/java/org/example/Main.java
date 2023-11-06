e4037346cb836544e3bc85cb39360736048b1766
Simeon <simeon.georev.97@hotmail.com>
2023-11-06 18:16:28 +0200

    Homework1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,19 +14,19 @@ public class Main {
         System.out.println("Nice to meet you " + userName + "!" );
 
         System.out.println("What is your preferred hobby " + userName + "?" );
-        System.out.println("Please choose from: 1 for Hiking , 2 for Swimming , 3 for Rock climbing , 4 for Playing video games , 5 for Football. ");
+        System.out.println("Please choose a number from: 1 for Hiking , 2 for Swimming , 3 for Rock climbing , 4 for Playing video games , 5 for Football. ");
 
-        int userHobie = scanner.nextInt();
+        int userHobby = scanner.nextInt();
 
-        if (userHobie == 1 ) {
-            System.out.println("That is my favorite.");
-        }if (userHobie == 2) {
+        if (userHobby == 1 ) {
+            System.out.println("Hiking is my favorite hobby.");
+        }if (userHobby == 2) {
             System.out.println("I used to love swimming.");
-        }if (userHobie == 3) {
-            System.out.println("If it is indoor it's cool but im scared of outdoor Rock climbing.");
-        }if (userHobie == 4) {
+        }if (userHobby == 3) {
+            System.out.println("Rock climbing is really cool hobby.");
+        }if (userHobby == 4) {
             System.out.println("Whats your favorite Video Game ?");
-        }if (userHobie == 5){
+        }if (userHobby == 5){
             System.out.println("How good are you at football ?");
         }else {
             System.out.println("Please run the code again so that you would not break it and next time use the provided numbers!");
